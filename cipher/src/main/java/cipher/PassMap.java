e92bb1c1d2819c54b596f0edbb0064dc30db496b
wolverineisking <logancollier04@gmail.com>
2020-01-10 06:50:07 -0800

    ghgj

--- cipher/src/main/java/cipher/PassMap.java
+++ cipher/src/main/java/cipher/PassMap.java
@@ -1,8 +1,12 @@
 package cipher;
 
 import java.util.HashMap;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
-public class PassMap {
+public class PassMap implements java.io.Serializable{
 	private HashMap<String,String> codex;
 	
 	public PassMap() {
@@ -13,5 +17,22 @@ public class PassMap {
 	}
 	public String getPass(String site) {
 		return codex.get(site);
+	}
+	@SuppressWarnings("unchecked")
+	private void loadCodex() {
+	      try {
+	         FileInputStream fileIn = new FileInputStream("/tmp/employee.ser");
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         this.codex = (HashMap<String,String>) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      } catch (IOException i) {
+	         i.printStackTrace();
+	         return;
+	      } catch (ClassNotFoundException c) {
+	         System.out.println("Employee class not found");
+	         c.printStackTrace();
+	         return;
+	      }
 	}
 }
